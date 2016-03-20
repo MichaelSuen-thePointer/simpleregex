406615ce53eb4757f72754cbffccd234921980ca
MichaelSuen <crazy95sun@live.cn>
2016-03-20 19:50:28 +0800

    checkpoint

--- simpleregex/lexfilereader.h
+++ simpleregex/lexfilereader.h
@@ -7,6 +7,8 @@
 #include <fstream>
 #include <sstream>
 #include <algorithm>
+
+#include "LexReaderTokenizer.h"
 namespace pl
 {
 using std::string;
@@ -16,6 +18,16 @@ string operator""_s(const char* str, size_t)
 {
     return string(str);
 }
+
+class ParseError: public std::runtime_error
+{
+public:
+    ParseError(const string& msg)
+        : std::runtime_error(msg)
+    {
+    }
+};
+
 class LexReader
 {
 public:
@@ -28,8 +40,16 @@ public:
 protected:
     string _className;
     string _classNameUpperCase;
+    char _delimiter;
     vector<Rule> _rules;
 
+    void expect(LexReaderTokenizer::Token& token, const LexReaderTokenizer::TokenKind kind, const string& description)
+    {
+        if (token.kind != kind)
+        {
+            throw ParseError("expect token: \'" + description + "\'");
+        }
+    }
 public:
     const string& class_name() { return _className; }
     const string& class_name_upper_case() { return _classNameUpperCase; }
@@ -37,56 +57,49 @@ public:
 
     void read_file(const string& filePath)
     {
-        std::ifstream lexFile(filePath);
-        constexpr size_t bufsize = 256;
-        char lineBuf[bufsize];
-        while (!lexFile.eof())
+        LexReaderTokenizer tokenizer(filePath);
+        while (tokenizer.peek().kind != tokenizer.End)
         {
-
-            lexFile.getline(lineBuf, bufsize);
-
-            size_t length = strlen(lineBuf);
-            if (length == 0)
+            auto token = tokenizer.get();
+            if (token.kind == tokenizer.ClassName)
             {
-                continue;
+                token = tokenizer.get();
+                expect(token, tokenizer.Colon, ":");
+                token = tokenizer.get();
+                expect(token, tokenizer.Regex, "a name");
+                _className = token.content;
+                _classNameUpperCase = _className;
+                std::transform(_className.begin(), _className.end(), _classNameUpperCase.begin(), [](char ch) {return ::toupper(ch); });
             }
-
-            std::istringstream line(lineBuf);
-
-            line.getline(lineBuf, bufsize, ':');
-            if (lineBuf == "ClassName"_s)
+            if (token.kind == tokenizer.Delimiter)
             {
-                line.getline(lineBuf, bufsize);
-                _className = lineBuf;
-                _classNameUpperCase = lineBuf;
-                std::transform(_className.begin(), _className.end(), _classNameUpperCase.begin(), ::toupper);
+                token = tokenizer.get();
+                expect(token, tokenizer.Colon, ":");
+                token = tokenizer.get();
+                expect(token, tokenizer.Regex, "a delimiter");
+                _delimiter = token.content[0];
             }
-            else if (lineBuf == "Rules"_s)
+            if (token.kind == tokenizer.Rules)
             {
-                read_rules(lexFile);
-                return;
+                token = tokenizer.get();
+                expect(token, tokenizer.Colon, ":");
+                read_rules(tokenizer);
             }
         }
     }
-    void read_rules(std::ifstream& lexFile)
+    void read_rules(LexReaderTokenizer& tokenizer)
     {
-        constexpr size_t bufsize = 256;
-        char lineBuf[bufsize];
-
-        while (!lexFile.eof())
+        int label = 1;
+        while (tokenizer.peek().kind != tokenizer.End)
         {
-            lexFile.getline(lineBuf, bufsize);
-            if (strlen(lineBuf) == 0)
-            {
-                continue;
-            }
-            std::istringstream sstream(lineBuf);
-            sstream.getline(lineBuf, bufsize, '\t');
-            string regex(lineBuf);
-            sstream.getline(lineBuf, bufsize);
-            string name(lineBuf);
+            auto regex = tokenizer.get();
+            expect(regex, tokenizer.Regex, "regex");
+            expect(tokenizer.get(), tokenizer.Colon, ":");
+            auto ruleName = tokenizer.get();
+            expect(ruleName, tokenizer.Regex, "a name");
 
-            _rules.push_back(Rule{static_cast<int>(_rules.size() + 1), name, regex});
+            _rules.push_back(Rule{label, regex.content, ruleName.content});
+            label++;
         }
     }
 };

42771adf600dd078dea20ff3e633838803ba0423
MichaelSuen <crazy95sun@live.cn>
2016-05-12 13:55:02 +0800

    Starting unit test

--- LexTest/main.cpp
+++ LexTest/main.cpp
@@ -1,22 +1,8 @@
+
 #include <iostream>
-#include "..\simpleregex\Tokenizer.h"
+#include <boost/test.hpp>
 
 int main()
 {
-    Tokenizer lex("keywordTest.txt");
-
-    try
-    {
-        while (lex.peek().kind != lex.EndToken)
-        {
-
-            auto token = lex.get();
-
-            std::cout << token.kind << ' ' << token.content << ' ' << token.line << ' ' << token.column << '\n';
-        }
-    }
-    catch (BadToken bd)
-    {
-        std::cout << bd.line << ' ' << bd.column << ' ' << bd.content << '\n';
-    }
+    
 }
